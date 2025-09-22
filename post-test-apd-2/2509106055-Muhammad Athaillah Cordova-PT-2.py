cb65089e637a8e3e219b3fab2f2f86fa045cf541
Cordoa88 <athaillah.cordova@gmail.com>
2025-09-22 11:50:34 +0800

    nambah variabel dan rumus

--- post-test-apd-2/2509106055-Muhammad Athaillah Cordova-PT-2.py
+++ post-test-apd-2/2509106055-Muhammad Athaillah Cordova-PT-2.py
@@ -1,1 +1,9 @@
-suhu = [27, 33, 46, 55, 67, 92]
+suhu = float([27, 33, 46, 55, 67, 92])
+farhenheit = (9/5) * suhu[0:3] + 32
+kelvin = suhu[3:4] + 273,15
+reamur = (4/5) * suhu[4: ]
+
+print (suhu)
+print (farhenheit)
+print (kelvin)
+print (reamur)
