6d69ecefd1662a839a70bcbf65de0a2c52dfcd09
Cordoa88 <athaillah.cordova@gmail.com>
2025-09-22 18:06:07 +0800

    Final

--- post-test-apd-2/2509106055-Muhammad Athaillah Cordova-PT-2.py
+++ post-test-apd-2/2509106055-Muhammad Athaillah Cordova-PT-2.py
@@ -1,9 +1,36 @@
-suhu = float([27, 33, 46, 55, 67, 92])
-farhenheit = (9/5) * suhu[0:3] + 32
-kelvin = suhu[3:4] + 273,15
-reamur = (4/5) * suhu[4: ]
+print("=== Ini adalah suhu dalam Celcius ===")
+suhu = [27, 33, 46, 55, 67, 92]
+
+# rumus konversi Celcius ke Farhenheit
+farhenheit1 = (9/5) * suhu[0] + 32
+farhenheit2 = (9/5) * suhu[1] + 32
+# rumus konversi Celcius ke Kelvin
+kelvin1 = suhu[-4] + 273.15
+kelvin2 = suhu[-3] + 273.15
+# rumus konversi Celcius ke Reamur
+reamur1 = (4/5) * suhu[-2]
+reamur2 = (4/5) * suhu[-1]
 
 print (suhu)
-print (farhenheit)
-print (kelvin)
-print (reamur)
+
+print("== Hasil konversi suhu ke-1 dan ke-2 dari Celcius ke Farhenheit ==")
+print(f"{suhu[0]} Celcius = {farhenheit1} Farhenheit")
+print(f"{suhu[1]} Celcius = {farhenheit2} Farhenheit")
+
+print("== Hasil konversi suhu ke-3 dan ke-4 dari Celcius ke Kelvin ==")
+print(f"{suhu[-4]} Celcius = {kelvin1} Kelvin")
+print(f"{suhu[-3]} Celcius = {kelvin2} Kelvin")
+
+print("== Hasil konversi suhu ke-5 dan ke-6 dari Celcius ke Reamur ==")
+print(f"{suhu[-2]} Celcius = {reamur1} Reamur")
+print(f"{suhu[-1]} Celcius = {reamur2} Reamur")
+
+jumlah = farhenheit1+farhenheit2+kelvin1+kelvin2+reamur1+reamur2
+rata2 =  jumlah/len(suhu)
+
+print (f"Jumlah suhu setelah di konversi adalah {jumlah}")
+print (f"Rata-rata dari jumlah keenam suhu tadi adalah {rata2}")
+
+NIM = 55
+boolean = NIM < rata2
+print(boolean)
